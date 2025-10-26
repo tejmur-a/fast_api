1b09dd7689a0c236962b52ef55ffc77afa4c23fd
Your Name <tejmur.aliev@icloud.com>
2025-10-26 22:42:33 +0300

    fix some bugs hosts

--- schemas.py
+++ schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from pydantic import ConfigDict
 
 class STaskAdd(BaseModel):
     name: str
@@ -7,6 +8,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
 
 class STaskId(BaseModel):
     ok: bool = True
